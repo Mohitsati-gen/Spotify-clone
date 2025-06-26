0b32a71c365982a17e3197a0cb4c2e805a3f1f39
Mohit-sati <abhisheksati987@gmail.com>
2025-06-26 12:40:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ async function getSongs(folder) {
 }
 
 const playMusic = (track, pause = false) => {
-    currentSong.src = `/${currFolder}/` + track
+    currentSong.src = `${currFolder}/` + track
     if (!pause) {
         currentSong.play()
         play.src = "./img/pause.svg"
